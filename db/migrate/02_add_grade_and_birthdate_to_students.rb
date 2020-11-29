1dbf583210da0c65b9e9fa37d73a362585ae5029
Samara Grant <samara.amanda01@gmail.com>
2020-11-29 08:14:12 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,4 +1,4 @@
-class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.2]
+class AddGradeAndBirthdateToStudents < ActiveRecord::Migration
   #def change
     #add_column :students do |t|
       #t.integer :grade

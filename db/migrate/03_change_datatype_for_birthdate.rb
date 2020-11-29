accee8caee1c0929c3ed8fcc57e7b98e725adced
Samara Grant <samara.amanda01@gmail.com>
2020-11-29 08:22:21 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,7 +1,7 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.2]
   def change
     change_column(table_name, column_name, type) :students do |student|
-      student.integer :birthdate
+      student.integer :datetime
     end
   end
 end
